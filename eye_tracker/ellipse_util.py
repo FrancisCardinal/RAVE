57fcdb824e111785d925e23cdbe43da3c131b634
Francis Cardinal <francis.cardinal.01@gmail.com>
2021-07-11 01:22:56 -0400

    Ajout de tests sommaires pour la fonction de coût

--- eye_tracker/ellipse_util.py
+++ eye_tracker/ellipse_util.py
@@ -1,29 +1,33 @@
 import torch
 from torch import sin, cos
+import cv2
+import numpy as np
 from numpy import pi 
-from tqdm import tqdm
+from numpy import testing
+
 
 def ellipse_loss_function(predictions, targets):
     NUMBER_OF_POINTS = 100
-    DEVICE = predictions.device
 
-    predicted_points = get_points_of_ellipses(predictions, DEVICE, NUMBER_OF_POINTS)
-    target_points    = get_points_of_ellipses(targets, DEVICE, NUMBER_OF_POINTS)
+    predicted_points = get_points_of_ellipses(predictions, NUMBER_OF_POINTS)
+    target_points    = get_points_of_ellipses(targets, NUMBER_OF_POINTS)
 
-    distance = ((predicted_points-target_points)**2).sum()
+    distance = (torch.sqrt( (predicted_points-target_points)**2) ).sum()
     mean_distance = distance / NUMBER_OF_POINTS
 
     return mean_distance
 
-def get_points_of_ellipses(ellipses, device, NUMBER_OF_POINTS): 
+def get_points_of_ellipses(ellipses, NUMBER_OF_POINTS): 
+    DEVICE = ellipses.device
+
     NUMBER_OF_ELLIPSES = ellipses.size()[0]
-    output_points = torch.empty((NUMBER_OF_ELLIPSES, NUMBER_OF_POINTS, 2), device=device)
+    output_points = torch.empty((NUMBER_OF_ELLIPSES, NUMBER_OF_POINTS, 2), device=DEVICE)
 
     current_point_index = 0 
     for ellipse in ellipses: 
         h, k, a, b, theta = ellipse
         theta = theta*2*pi 
-        points = get_points_of_an_ellipse(h, k, a, b, theta, device, NUMBER_OF_POINTS)
+        points = get_points_of_an_ellipse(h, k, a, b, theta, DEVICE, NUMBER_OF_POINTS)
 
         output_points[current_point_index] = points
         current_point_index += 1 
@@ -31,7 +35,7 @@ def get_points_of_ellipses(ellipses, device, NUMBER_OF_POINTS):
     return output_points
 
 
-def get_points_of_an_ellipse(h, k, a, b, theta, device, NUMBER_OF_POINTS = 300):
+def get_points_of_an_ellipse(h, k, a, b, theta, device, NUMBER_OF_POINTS):
     # J'utilise les coordonnées polaires pour générer les points qui appartiennent à l'ellipse ; voir https://math.stackexchange.com/questions/2645689/what-is-the-parametric-equation-of-a-rotated-ellipse-given-the-angle-of-rotatio 
     output_points = torch.empty((NUMBER_OF_POINTS, 2), device=device)
 
@@ -46,3 +50,35 @@ def get_points_of_an_ellipse(h, k, a, b, theta, device, NUMBER_OF_POINTS = 300):
         current_point_index += 1 
     
     return output_points
+
+
+def draw_ellipse_on_image(image, ellipse, color=(255, 0, 0), thickness=2):
+    _, HEIGHT, WIDTH = image.shape
+    h, k, a, b, theta = ellipse 
+    h, k, a, b = h*WIDTH, k*HEIGHT, a*WIDTH, b*HEIGHT
+    theta = theta*2*pi 
+    points = get_points_of_an_ellipse(h, k, a, b, theta, ellipse.device, 360)
+    points = points.cpu().numpy()
+
+    image = cv2.polylines(image, np.int32([points]), True, color, thickness)
+
+    return image
+
+
+if __name__=='__main__':
+    DEVICE = 'cpu'
+    if( torch.cuda.is_available() ): 
+        DEVICE = 'cuda'
+
+    with torch.no_grad():
+        x = torch.tensor([[0.1, 0.2, 0.3, 0.4, 0.5], [0.6, 0.7, 0.8, 0.9, 1.0]], device=DEVICE)
+        y = torch.tensor([[0.1, 0.2, 0.3, 0.4, 0.5], [0.6, 0.7, 0.8, 0.9, 1.0]], device=DEVICE)
+        testing.assert_allclose ( ellipse_loss_function(x, y).cpu(), 0 )
+
+        x = torch.tensor([[0.1, 0.2, 0.3, 0.4, 0.5], [0.6, 0.7, 0.8, 0.9, 1.0]], device=DEVICE)
+        y = torch.tensor([[0.2, 0.2, 0.3, 0.4, 0.5], [0.6, 0.7, 0.8, 0.9, 1.0]], device=DEVICE)
+        testing.assert_allclose ( ellipse_loss_function(x, y).cpu(), 0.1  )
+
+        x = torch.tensor([[0.1, 0.3, 0.3, 0.4, 0.5], [0.6, 0.7, 0.8, 0.9, 1.0]], device=DEVICE)
+        y = torch.tensor([[0.2, 0.2, 0.3, 0.4, 0.5], [0.6, 0.7, 0.8, 0.9, 1.0]], device=DEVICE)
+        testing.assert_allclose ( ellipse_loss_function(x, y).cpu(), 0.2  )
