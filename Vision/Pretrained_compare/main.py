b3a26fb1ff8cdb8faf04e570d780a51ea3cb5cd7
Anthony Gosselin <anthony.gosselin2@usherbrooke.ca>
2021-07-19 12:11:16 -0400

    Feed multiple images at once

--- Vision/Pretrained_compare/main.py
+++ Vision/Pretrained_compare/main.py
@@ -1,4 +1,6 @@
 import cv2
+import glob
+import os
 
 from resources.fpsHelper import FPS
 from models import haar, hog, dnn
@@ -32,13 +34,28 @@ def stream_detect(detect_func):
     cv2.destroyAllWindows()
 
 
+def feed_images_detect(detect_func, in_folder="images_in/", out_folder="images_out/"):
+
+    # Clear out folder
+    for old_img in glob.glob(out_folder + "*"):
+        os.remove(old_img)
+
+    # Load, detect and save images in input folder
+    for img in glob.glob(in_folder + "*"):
+        final_img = detect_func(cv2.imread(img))
+        cv2.imwrite(out_folder + os.path.basename(img), final_img)
+
+
 if __name__ == '__main__':
 
     # Test with image
     # image_detect("resources/TonyFace1.jpg")
 
     # Test with video stream
-    stream_detect(dnn.detect_faces)
+    # stream_detect(dnn.detect_faces)
+
+    # Feed multiple images to detect
+    feed_images_detect(dnn.detect_faces)
 
 
 
