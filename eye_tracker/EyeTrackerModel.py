090e596ee5eeb4d12e16b5f5ad3ccb1d64534738
Francis Cardinal <francis.cardinal.01@gmail.com>
2021-07-22 13:20:49 -0400

    Meilleures performances du modèle : 2021-07-22 09:13:34 | ellapsed_time=12:17:13.437762 | min_validation_loss=0.021920 | min_training_loss=0.022954

--- eye_tracker/EyeTrackerModel.py
+++ eye_tracker/EyeTrackerModel.py
@@ -16,7 +16,7 @@ class EyeTrackerModel(nn.Module):
 
         n_inputs = self.model.fc.in_features
 
-        DROPOUT = 0.05
+        DROPOUT = 0.075
         self.model.fc = nn.Sequential(
             nn.Linear(n_inputs, 512),
             nn.BatchNorm1d(num_features=512),
