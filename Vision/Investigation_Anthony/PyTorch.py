6d85cd3a248cbcb08b159ef0fae082fda2869c12
felixducharme1 <felix@revtechsys.com>
2021-07-28 12:13:29 -0400

    Clean Up branch

--- Vision/Investigation_Anthony/PyTorch.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-print("Torch version: " + torch.__version__)
-
-cuda_available = torch.cuda.is_available()
-print(f"Is CUDA available: {cuda_available}")
-
-if cuda_available:
-    print(f"CUDA version: {torch.version.cuda}")
