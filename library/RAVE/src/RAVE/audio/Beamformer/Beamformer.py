401288b03d0ce3a48962d6190936717713969a36
Wazqaz <etienne.deshaies-samson@usherbrooke.ca>
2022-01-13 12:37:35 -0500

    Added empty classes and documentation...?

--- library/RAVE/src/RAVE/audio/Beamformer/Beamformer.py
+++ library/RAVE/src/RAVE/audio/Beamformer/Beamformer.py
@@ -8,10 +8,23 @@ BEAMFORMER_TYPES = {
 
 
 class Beamformer:
+    """
+    Class that handles Beamformer type abstraction and all external calls
+    See PyOdas beamformer documentation <https://introlab.github.io/pyodas/_build/html/pyodas/core/beamformers.html>.
+
+    Args:
+        name (str): Beamformer type name (delaysum, mvdr, gev)
+        **kwargs: Keyword arguments relative to the initialization of the various beamformers
+    """
 
     def __init__(self, name, **kwargs):
         self.beamformer_type = name.lower()
         self.beamformer = BEAMFORMER_TYPES[name.lower()](**kwargs)
 
     def __call__(self, **kwargs):
+        """
+        Execute beamformer implementation (execution depends on beamformer chosen)
+        See PyOdas beamformer documentation
+        <https://introlab.github.io/pyodas/_build/html/pyodas/core/beamformers.html>.
+        """
         return self.beamformer(**kwargs)
