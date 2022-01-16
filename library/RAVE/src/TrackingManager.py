1d7fd4de7605b021fdc560e66f1c61984d5b4fa4
Jacob Kealey <jacob.kealey@usherbrooke.ca>
2022-01-16 15:00:33 -0500

    Adding ids to tracking and adaptive scaling

--- library/RAVE/src/TrackingManager.py
+++ library/RAVE/src/TrackingManager.py
@@ -1,27 +1,36 @@
+import time
 import cv2
 import threading
+import uuid
 
 from trackers import TrackerFactory
 from face_detectors import YoloFaceDetector
+from RAVE.common.image_utils import intersection
 from RAVE.face_detection.fpsHelper import FPS
-from pyodas.visualize import VideoSource
+from pyodas.visualize import VideoSource, Monitor
 
 
 class TrackedObject:
-    def __init__(self, tracker, bbox):
+    def __init__(self, tracker, bbox, identifier):
         self.tracker = tracker
         self.bbox = bbox
-        # self._tracked_id = tracked_id  # TODO: set unique id
+        self._id = identifier
+
+    @property
+    def id(self):
+        return self._id
 
     def update_bbox(self, bbox):
         self.bbox = bbox
 
 
 class TrackingManager:
-    def __init__(self, tracker_type):
+    def __init__(self, tracker_type, frequency):
         self._tracker_type = tracker_type
 
-        self._tracked_objects = []
+        self._tracked_objects = {}
+
+        self._frequency = frequency
         self.count = 0
 
         self._detector = YoloFaceDetector()
@@ -30,19 +39,23 @@ class TrackingManager:
     def tracking_count(self):
         return len(self._tracked_objects)
 
-    def add_tracked_object(self, frame, bbox):
+    def add_tracked_object(self, frame, bbox, identifier=None):
         new_tracker = TrackerFactory.create(self._tracker_type)
-        new_tracked_object = TrackedObject(new_tracker, bbox)
-        self._tracked_objects.append(new_tracked_object)
+        new_id = str(identifier) if identifier else str(uuid.uuid4())
+        new_tracked_object = TrackedObject(new_tracker, bbox, new_id)
+        self._tracked_objects[new_tracked_object.id] = new_tracked_object
 
         new_thread = threading.Thread(
-            target=self.track_loop, args=(new_tracked_object,)
+            target=self.track_loop, args=(new_tracked_object,), daemon=True
         )
-        new_tracker.start(frame, bbox)
+        new_tracked_object.tracker.start(frame, bbox)
         new_thread.start()
 
-    def remove_tracked_object(self, index):
-        self._tracked_objects.pop(index)
+    def remove_tracked_object(self, identifier):
+        self._tracked_objects.pop(identifier)
+
+    def stop_tracking(self):
+        self._tracked_objects = {}
 
     def track_loop(self, tracked_object):
 
@@ -58,10 +71,8 @@ class TrackingManager:
                 xywh_rect = [int(v) for v in box]
                 tracked_object.update_bbox(xywh_rect)
 
-        print("Track loop ended")
-
-    def listen_keyboard_input(self, frame):
-        key = cv2.waitKey(30) & 0xFF
+    def listen_keyboard_input(self, frame, key_pressed):
+        key = key_pressed & 0xFF
         if key == ord("s"):
             # Select object to track manually
             selected_roi = cv2.selectROI(
@@ -71,17 +82,21 @@ class TrackingManager:
         elif key == ord("x"):
             # Remove last tracked object
             if len(self._tracked_objects) > 0:
-                self._tracked_objects.pop()
-        elif key == 27 or key == ord("q"):
-            # Stop if escape key is pressed
-            return True
+                self._tracked_objects.popitem()
 
     def start(self):
-        cap = VideoSource(0)
+        cap = VideoSource(4)
+        m = Monitor(
+            "Detection", cap.shape, "Tracking", cap.shape, refresh_rate=30
+        )
+        cap.set(cv2.CAP_PROP_FPS, 60)
         fps = FPS()
 
+        start_time = time.time()
         # Main display loop
-        while True:
+        while m.window_is_alive():
+            # TODO (JKealey): Assign directly to sel._last_frame
+            #  and add mutex(?)
             frame = cap()
             self._last_frame = frame
 
@@ -95,32 +110,71 @@ class TrackingManager:
                     frame.copy(), draw_on_frame=True
                 )
                 if len(predicted_bboxes) > 0:
-                    # TODO (Anthony): choose a better criterion for selecting
-                    # bbox (in case there are more than one)
-                    self.add_tracked_object(frame, predicted_bboxes[0])
-                    cv2.imshow("initial bbox", face_frame)
+                    for predicted_bbox in predicted_bboxes:
+                        self.add_tracked_object(frame, predicted_bbox)
+                    m.update("Detection", face_frame)
+
+            elif time.time() - start_time >= self._frequency:
+                face_frame, predicted_bboxes = self._detector.predict(
+                    frame.copy(), draw_on_frame=True
+                )
+                if len(predicted_bboxes) > 0:
+                    # TODO (JKealey): Find a way to not declare new objects,
+                    #  maybe with our own implementation
+
+                    for predicted_bbox in predicted_bboxes:
+                        # TODO (JKealey): Find a better way to link previous
+                        #  ids to the new bboxes
+                        intersection_scores = {}
+                        for (
+                            tracker_id,
+                            tracked_object,
+                        ) in self._tracked_objects.items():
+                            intersection_scores[tracker_id] = intersection(
+                                predicted_bbox, tracked_object.bbox
+                            )
+
+                        max_id = max(
+                            intersection_scores, key=intersection_scores.get
+                        )
+
+                        if intersection_scores[max_id]:
+                            new_id = max_id
+                            self.remove_tracked_object(max_id)
+                        else:
+                            new_id = None
+
+                        self.add_tracked_object(frame, predicted_bbox, new_id)
+                    m.update("Detection", face_frame)
 
             # Draw bboxes from tracked objects
-            for tracked_object in self._tracked_objects:
+            for tracked_object in self._tracked_objects.values():
                 if tracked_object.bbox is None:
                     continue
                 x, y, w, h = tracked_object.bbox
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                cv2.putText(
+                    frame,
+                    tracked_object.id,
+                    (x, y - 2),
+                    0,
+                    1 / 3,
+                    [225, 255, 255],
+                    thickness=1,
+                    lineType=cv2.LINE_AA,
+                )
 
             # Update display
             fps.setFps()
             frame = fps.writeFpsToFrame(frame)
-            cv2.imshow("Tracking", frame)
+            m.update("Tracking", frame)
 
             # Keyboard input controls
-            terminate = self.listen_keyboard_input(frame)
-            if terminate:
-                break
+            self.listen_keyboard_input(frame, m.key_pressed)
 
-        cap.stop()
-        cv2.destroyAllWindows()
+        self.stop_tracking()
 
 
 if __name__ == "__main__":
-    tracking_manager = TrackingManager(tracker_type="kcf")
+    tracking_manager = TrackingManager(tracker_type="kcf", frequency=1)
     tracking_manager.start()
