691494449bd98265ed194c524d096052c5ad6255
Wazqaz <etienne.deshaies-samson@usherbrooke.ca>
2022-01-17 16:47:26 -0500

    Started dataset generation code

--- library/RAVE/src/RAVE/audio/Dataset/AudioDatasetBuilder.py
+++ library/RAVE/src/RAVE/audio/Dataset/AudioDatasetBuilder.py
@@ -1,8 +1,205 @@
+import glob
+import os
+import rir_generator as rir
+import yaml
+import numpy as np
+from scipy import signal
+import soundfile as sf
+
+from pyodas.utils import TYPES
+from pyodas.core import SpatialCov
+
+SAMPLE_RATE = 16000
+
 
 class AudioDatasetBuilder:
     """
     Class which handles the generation of the audio dataset through the randomization of sources and various
     parameters passed through DatasetBuilder_config and input_path yaml files.
+
+    Args:
+        dataset_size (int): Number of dataset items to generate
+
     """
 
-    pass
+    r = [               # Receiver (microphone) positions [x, y, z] (m)
+
+    ]
+    c = 340             # Sound velocity (m/s)
+    reverb_time = 0.4   # Reverberation time (s)
+    nsample = 4096      # Number of output samples
+
+    def __init__(self, max_sources, sources_path, output_path, noises_path):
+        self.max_sources = max_sources      # Maximum number of sources in one output segment
+
+        # Load params/configs
+        with open("DatasetBuilder_config.yaml", "r") as stream:
+            try:
+                self.configs = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+        self.positions = self.configs['position']
+        self.rooms = self.configs['room']
+
+        # Load input sources paths (speech, noise)
+        self.source_paths = glob.glob(os.path.join(sources_path, '**', '*.wav'))
+        self.noise_paths = glob.glob(os.path.join(noises_path, '**', '*.wav'))
+
+        # Prepare output subfolder
+        self.output_subfolder = output_path
+        if os.path.exists(self.output_subfolder):
+            # TODO: Add functionality if existing output folder
+            print(f"ERROR: Output folder '{self.output_subfolder}' already exists, exiting.")
+            exit()
+
+
+    @staticmethod
+    def read_audio_file(file_path):
+        """
+        Reads and extracts audio from a file. Uses PyODAS WavSource class
+        <https://introlab.github.io/pyodas/_build/html/pyodas/io/io.sources.html#module-pyodas.io.sources.wav_source>.
+
+        Args:
+            file_path (str): Path to audio file
+
+        Returns:
+            audio_signal (ndarray): Audio read from path of length chunk_size
+            sample_frequency (int): Audio file sample frequency
+        """
+        audio_signal = sf.read(file_path, samplerate=SAMPLE_RATE)
+        return audio_signal
+
+    @staticmethod
+    def apply_rir(rirs, signal):
+        """
+        Method to apply RIRs to mono signals.
+
+        Args:
+            rirs (ndarray): RIRs generated previously to reflect room, source position and mic positions
+            signal (ndarray): Monochannel signal to be reflected to multiple microphones
+
+        Returns:
+            output(ndarray): Every RIR applied to monosignal
+        """
+        channels = rirs.shape[0]
+        frames = signal.shape[0]
+        output = np.empty((channels, frames))
+
+        for channel_index in range(channels):
+            output[channel_index] = signal.convolve(signal, rir[channel_index])[:frames]
+        return output
+
+    @staticmethod
+    def combine_sources(source, noises, channels=6):
+        """
+        Method used to combine audio source with noises.
+
+        Args:
+            source (list[ndarray]): Source to add noises to. Shape is (channels, [signal])
+            noises (list[list[ndarray]]): List of noises to add to source. Shape is (# noises, channels, [signal])
+            channels (int): Number of audio channels.
+
+        Returns:
+            combined_audio: Combined source with noises. Shape is like source (channels, [signal])
+        """
+        # Get length of shortest audio
+        array_lengths = [len(source[0])]
+        for noise_array in noises:
+            array_lengths.append(len(noise_array))
+        max_length = min(array_lengths)
+
+        combined_audio = source
+        for noise in noises:
+            for combined_channel, noise_channel in zip(combined_audio, noise):
+                combined_channel += noise_channel[:max_length]
+
+        return combined_audio
+
+    @staticmethod
+    def generate_ground_truth(x):
+        """
+        Determines the spatial covariance matrix of input signal to use as ground truth for dataset.
+
+        Args:
+            signal (ndarray): Signal on which to get spatial covariance matrix.
+
+        Returns:
+            spatial_covariance: Spatial covariance of signal.
+        """
+        X = np.fft.rfft(x)
+        channels, bins = X.shape
+        # TODO: Change scm for stft
+        scm = SpatialCov(channels, (bins-1)*2)
+        spatial_covariance = scm(X)
+
+        return spatial_covariance
+
+    def save_files(self, combined_signal, combined_gt, source_name, source_gt,
+                   noise_names, noise_gts, combined_noise_gt):
+        """
+        Save various files needed for dataset.
+
+        Args:
+            combined_signal: Audio signal array of sources and noise together, 1 channel per receiver.
+            combined_gt: STFT of combined audio signal.
+            source_name: Name of source sample used.
+            source_gt: STFT of source signal.
+            noise_names: List of names of noise samples.
+            noise_gts: List of STFT of noise signals.
+            combined_noise_gt: STFT of combined noise signals.
+
+        Returns:
+            subfolder_path (str): String containing path to newly created dataset subfolder.
+        """
+        # Create subfolder
+        subfolder_path = os.path.join(self.output_subfolder, '1', source_name)
+        for noise_name in noise_names:
+            subfolder_path = os.path.join(subfolder_path, noise_name)
+        os.makedirs(subfolder_path, exist_ok=True)
+
+        # Save audio file and audio ground truth
+        audio_file_name = os.path.join(subfolder_path, 'combined_audio.wav')
+        sf.write(audio_file_name, combined_signal, SAMPLE_RATE)
+        audio_gt_name = os.path.join(subfolder_path, 'combined_audio_scm.npz')
+        np.savez_compressed(audio_gt_name, combined_gt)
+
+        # Save source ground truth
+        source_gt_name = os.path.join(subfolder_path, f'{source_name}_gt.npz')
+        np.savez_compressed(source_gt_name, source_gt)
+
+        # Save noise and combined noise ground truths
+        combined_noise_gt_name = os.path.join(subfolder_path, f'combined_noise_gt.npz')
+        np.savez_compressed(combined_noise_gt_name, combined_noise_gt)
+        for noise_name, noise_gt in zip(noise_names, noise_gts):
+            noise_gt_name = os.path.join(subfolder_path, f'{noise_name}_gt.npz')
+            np.savez_compressed(noise_gt_name, noise_gt)
+
+        return subfolder_path
+
+    def generate_and_apply_rirs(self, source_path, source_pos, room):
+        """
+        Function that generates Room Impulse Responses (RIRs) to source signal positions and applies them to signals.
+        See <https://pypi.org/project/rir-generator/>. Will have to look later on at
+        <https://github.com/DavidDiazGuerra/gpuRIR#simulatetrajectory> for moving sources.
+
+        Args:
+            source_path (str): Path to audio file.
+            source_pos (list[float, float, float]): Source position ([x y z] (m))
+            room (list[float, float, float]): Room dimensions ([x y z] (m))
+
+        Returns:
+            source_with_rir: Source signal with RIRs applied (shape is [channels,
+
+        """
+        s_signal = self.read_audio_file(source_path)
+        rirs = rir.generate(
+            c=self.c,                               # Sound velocity (m/s)
+            fs=SAMPLE_RATE,                         # Sample frequency (samples/s)
+            r=self.r,                               # Receiver position(s) [x y z] (m)
+            s=source_pos,                           # Source position [x y z] (m)
+            L=room,                                 # Room dimensions [x y z] (m)
+            reverberation_time=self.reverb_time,    # Reverberation time (s)
+            nsample=self.nsample,                   # Number of output samples
+        )
+        source_with_rir = self.apply_rir(rirs, s_signal)
+        return source_with_rir
--- library/RAVE/src/RAVE/audio/generate_dataset.py
+++ library/RAVE/src/RAVE/audio/generate_dataset.py
@@ -1,1 +1,129 @@
+import argparse
+from tkinter import filedialog
+
+from ..audio import AudioDatasetBuilder
+
 # Script used to generate the audio dataset
+def main(SOURCES, NOISES, OUTPUT, MAX_SOURCES):
+    dataset_builder = AudioDatasetBuilder(MAX_SOURCES, SOURCES, OUTPUT, NOISES)
+
+    positions = dataset_builder.positions
+
+    # For each room
+    for room in dataset_builder.rooms:
+        # TODO: Check if noise and source audios are too big for memory
+        # TODO: Check if saving audios with RIRs are worth in long run
+
+        # Generate noises once for each room
+        noise_with_rirs_pos = []
+        for noise_pos in dataset_builder.positions:
+            noise_with_rirs = []
+            for noise_src in dataset_builder.noise_paths:
+                noise_name = noise_src.split('.')[-2]       # Get filename for noise
+                noise_with_rir = dataset_builder.generate_and_apply_rirs(noise_src, noise_pos, room)
+                noise_with_rirs.append((noise_name, noise_with_rir))
+            noise_with_rirs_pos.append(noise_with_rirs)
+
+        # Generate source files with rirs
+        source_with_rirs_pos = []
+        for source_pos in dataset_builder.positions:
+            source_with_rirs = []
+            for source_src in dataset_builder.source_paths:
+                source_name = source_src.split('.')[-2]       # Get filename for source
+                source_with_rir = dataset_builder.generate_and_apply_rirs(source_src, source_pos, room)
+                source_with_rirs.append((source_name, source_with_rir))
+            source_with_rirs_pos.append(source_with_rirs)
+
+        # Combine sources and noises
+        # TODO: Add option for multiple noise sources
+        for source_i, source_audio_pos_list in enumerate(source_with_rirs_pos):
+            for noise_i, noise_audio_pos_list in enumerate(noise_with_rirs_pos):
+                # Skip noises where position is on top of source
+                if noise_i != source_i:
+                    for source_audio in source_audio_pos_list:
+                        source_name = source_audio[0]
+                        source_signal = source_audio[1]
+                        source_gt = dataset_builder.generate_ground_truth(source_signal)
+                        for noise_audio in noise_audio_pos_list:
+                            # TODO: Add optimisation to stop calculating SCM on same combination of sounds
+                            noise_name = noise_audio[0]
+                            noise_name_list = [noise_name]
+                            noise_signal = noise_audio[1]
+                            noise_signal_list = [noise_signal]
+                            noise_gt = dataset_builder.generate_ground_truth(noise_signal)
+                            noise_gt_list = [noise_gt]
+                            combined_noise_gt = noise_gt
+                            combined_audio = dataset_builder.combine_sources(source_signal, noise_signal_list)
+                            combined_scm = dataset_builder.generate_ground_truth(combined_audio)
+
+                            dataset_builder.save_files(combined_audio, combined_scm,
+                                                       source_name, source_gt,
+                                                       noise_name_list, noise_gt_list, combined_noise_gt)
+
+
+
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    # parser.add_argument(
+    #     "-t", "--train", action="store_true", help="Train the neural network"
+    # )
+    parser.add_argument(
+        "-s",
+        "--sources",
+        action="store",
+        type=str,
+        default='tkinter',
+        help="Absolute path to audio sources",
+    )
+    parser.add_argument(
+        "-n",
+        "--noises",
+        action="store",
+        type=str,
+        default='tkinter',
+        help="Absolute path to noise sources",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        action="store",
+        type=str,
+        default='tkinter',
+        help="Absolute path to output dataset folder",
+    )
+
+    parser.add_argument(
+        "-m",
+        "--max_sources",
+        action="store",
+        type=int,
+        default=3,
+        help="Maximum number of interfering sources (and noises) to add",
+    )
+
+    args = parser.parse_args()
+
+    # parse sources
+    source_subfolder = args.sources
+    if source_subfolder == 'tkinter':
+        source_subfolder = filedialog.askdirectory()
+
+    # parse noises
+    noise_subfolder = args.noises
+    if noise_subfolder == 'tkinter':
+        noise_subfolder = filedialog.askdirectory()
+
+    # parse output
+    output_subfolder = args.output
+    if output_subfolder == 'tkinter':
+        output_subfolder = filedialog.askdirectory()
+
+    main(
+        source_subfolder,
+        noise_subfolder,
+        output_subfolder,
+        args.max_sources
+    )
