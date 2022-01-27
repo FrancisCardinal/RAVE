151274b1fb36e0c27660b774a25c72b078335cbe
Wazqaz <etienne.deshaies-samson@usherbrooke.ca>
2022-01-27 11:30:12 -0500

    Added possibility to have multiple sources/sinks

--- library/RAVE/src/RAVE/audio/IO/IO_manager.py
+++ library/RAVE/src/RAVE/audio/IO/IO_manager.py
@@ -19,27 +19,35 @@ class IOManager:
     """
 
     def __init__(self):
-        self.source = None
-        self.sink = None
+        self.source = {}
+        self.sink = {}
 
-    def init_source(self, source_type, **kwargs):
+    def add_source(self, source_name, source_type, **kwargs):
         """
-        Initialize source to use for input.
+        Add source to use for input.
         See PyODAS documentation <https://introlab.github.io/pyodas/_build/html/pyodas/io/io.sources.html>
 
         Args:
+            source_name (str): Name to use to refer to source (dict key)
             source_type (str): Input type to use as source (source_wav, source_mic)
             **kwargs: More keyword args are required depending on input type chosen, .
+        Return:
+            Source newly added to source dict
         """
-        self.source = SOURCE_TYPES[source_type.lower()](**kwargs)
+        self.source[source_name] = SOURCE_TYPES[source_type.lower()](**kwargs)
+        return self.source[source_name]
 
-    def init_sink(self, sink_type, **kwargs):
+    def init_sink(self, sink_name, sink_type, **kwargs):
         """
         Initialize sink to use for output.
         See PyODAS documentation <https://introlab.github.io/pyodas/_build/html/pyodas/io/io.sinks.html>
 
         Args:
+            sink_name (str): Name to use to refer to sink (dict key)
             sink_type (str): Output type to use as sink (sink_wav, sink_playback)
             **kwargs: More keyword args are required depending on output type chosen, .
+        Return:
+            Sink newly added to sink dict
         """
-        self.sink = SINK_TYPES[sink_type.lower()](**kwargs)
+        self.sink[sink_name] = SINK_TYPES[sink_type.lower()](**kwargs)
+        return self.sink[sink_name]
