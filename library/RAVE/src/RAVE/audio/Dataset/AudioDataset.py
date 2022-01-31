24b5caff1d131fcd14205bda738a3e0d3d2da4a8
Wazqaz <etienne.deshaies-samson@usherbrooke.ca>
2022-01-31 14:26:32 -0500

    Started dataset

--- library/RAVE/src/RAVE/audio/Dataset/AudioDataset.py
+++ library/RAVE/src/RAVE/audio/Dataset/AudioDataset.py
@@ -1,7 +1,23 @@
+import torch
 
-class AudioDataset:
+
+class AudioDataset(torch.utils.data.Dataset):
     """
-    Class handling usage of the audio dataset for training purposes.
+    Class handling usage of the audio dataset
     """
 
-    pass
+    def __init__(
+            self,
+            ROOT_PATH,
+            sub_dataset_dir
+    ):
+
+        BASE_PATH = os.path.join(
+            ROOT_PATH, Dataset.DATASET_DIR, sub_dataset_dir
+        )
+        self.IMAGES_DIR_PATH = os.path.join(BASE_PATH, Dataset.IMAGES_DIR)
+        self.LABELS_DIR_PATH = os.path.join(BASE_PATH, Dataset.LABELS_DIR)
+
+        self.images_paths = Dataset.get_multiple_workers_safe_list_of_paths(
+            self.IMAGES_DIR_PATH
+        )
