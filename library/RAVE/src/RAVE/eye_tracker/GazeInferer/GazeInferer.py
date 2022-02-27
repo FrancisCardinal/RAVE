Fixed Gaze Inferer so that it supports a rotation along the X angle

library/RAVE/src/RAVE/eye_tracker/GazeInferer/GazeInferer.py
@@ -13,7 +13,7 @@ uses pytorch instead of TF for predictions. As such, this is mostly copy and pas
 """
 
 class GazeInferer:
-    def __init__(self, ellipse_dnn, dataloader, device, eyeball_model_path = "model_01.json", image_scaling_factor=1,  pupil_radius=4, initial_eye_z=8, flen=3.37, sensor_size=(2.7216, 3.6288)):
+    def __init__(self, ellipse_dnn, dataloader, device, eyeball_model_path = "model_01.json", image_scaling_factor=1,  pupil_radius=4, initial_eye_z=49.497, x_angle=45, flen=3.37, sensor_size=(2.7216, 3.6288)):
         self._ellipse_dnn = ellipse_dnn 
         self._dataloader = dataloader
         self._device = device
@@ -26,12 +26,13 @@ class GazeInferer:
         #TODO FC : deal with image_scaling_factor when we'll have real images
         self._eyefitter = SingleEyeFitter(focal_length=flen * self.mm2px_scaling * image_scaling_factor,
                                     pupil_radius = pupil_radius * self.mm2px_scaling * image_scaling_factor,
-                                    initial_eye_z = initial_eye_z * self.mm2px_scaling,
+                                    initial_eye_z = initial_eye_z * self.mm2px_scaling * image_scaling_factor,
+                                    x_angle = x_angle,
                                     image_shape=self.shape)
 
     def fit(self):
         with torch.no_grad():
-            for images, _ in tqdm(self._dataloader, "fitting", leave=False):
+            for images, _ in tqdm(self._dataloader, "Adding to fitting", leave=False):
                 images = images.to(self._device)
 
                 # Forward Pass
@@ -42,12 +43,12 @@ class GazeInferer:
                     self._eyefitter.add_to_fitting()
 
         # Fit eyeball models. Parameters are stored as internal attributes of Eyefitter instance.
-        self._eyefitter.fit_projected_eye_centre(ransac=True, max_iters=5000, min_distance=2* len(self._dataloader.dataset))
+        self._eyefitter.fit_projected_eye_centre(ransac=True, max_iters=1000, min_distance=2* len(self._dataloader.dataset))
         self._eyefitter.estimate_eye_sphere()
 
         # Issue error if eyeball model still does not exist after fitting.
         if (self._eyefitter.eye_centre is None) or (self._eyefitter.aver_eye_radius is None):
-            raise TypeError("Eyeball model was not fitted. You may need -v or -m argument to check whether the pupil segmentation works properly.")
+            raise TypeError("Eyeball model was not fitted.")
         
         self.save_eyeball_model()
     
@@ -67,6 +68,7 @@ class GazeInferer:
 
     def infer(self):
         self.load_eyeball_model()
+        x_offset, y_offset = None, None
 
         with torch.no_grad():
             for images, _ in self._dataloader:
@@ -78,9 +80,15 @@ class GazeInferer:
                     self._eyefitter.unproject_single_observation(self.torch_prediction_to_deepvog_format(prediction))
                     _, n_list, _, _ = self._eyefitter.gen_consistent_pupil()
                     x, y = self._eyefitter.convert_vec2angle31(n_list[0])
+
+                    if(x_offset is None):
+                        x_offset = x 
+                        y_offset = y
+
+                    x -= x_offset
+                    y -= y_offset
+
                     print("x = {} y = {}".format(x,y))
-                    x -= 90
-                    y -= 90
 
     def load_eyeball_model(self):
         """

library/RAVE/src/RAVE/eye_tracker/GazeInferer/deepvog/eyefitter.py
@@ -11,13 +11,21 @@ All calculations are in camera frame (conversion would be commented)
 
 class SingleEyeFitter(object):
 
-    def __init__(self, focal_length, pupil_radius, initial_eye_z, image_shape):
+    def __init__(self, focal_length, pupil_radius, initial_eye_z, x_angle, image_shape):
         self.focal_length = focal_length
         self.image_shape = image_shape
 
         self.pupil_radius = pupil_radius
         self.vertex = [0, 0, -focal_length]
         self.initial_eye_z = initial_eye_z
+
+        self.camera_rotation_matrix = np.eye(3)
+        theta = np.deg2rad(x_angle) 
+        self.camera_rotation_matrix[1, 1] = np.cos(theta)
+        self.camera_rotation_matrix[2, 2] = np.cos(theta)
+        self.camera_rotation_matrix[2, 1] = np.sin(theta)
+        self.camera_rotation_matrix[1, 2] = -np.sin(theta)
+        self.camera_rotation_matrix = self.camera_rotation_matrix[0:2, 0:2]
 
         # (p,n) of unprojected gaze vector and pupil 3D position in SINGLE OBSERVATION
         self.current_gaze_pos = 0  # reserved for (3,1) np.array in camera frame
@@ -49,6 +57,13 @@ class SingleEyeFitter(object):
 
     def unproject_single_observation(self, observation):
         centre, w, h, radian = observation
+
+        centre = self.camera_rotation_matrix @ centre 
+
+        wh = [w, h]
+        wh = self.camera_rotation_matrix @ wh
+        w, h = wh[0], wh[1]
+
         centre_cam = centre.copy()
         centre_cam[0] = centre_cam[0] - self.image_shape[1] / 2
         centre_cam[1] = centre_cam[1] - self.image_shape[0] / 2