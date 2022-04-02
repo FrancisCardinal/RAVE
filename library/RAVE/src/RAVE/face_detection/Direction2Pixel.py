7b784cffae044621470f1ff569140880f0bc214a
Jacob Kealey <jacob.kealey@usherbrooke.ca>
2022-04-02 12:33:04 -0400

    Second way to compute the pixel postion from the direction

--- library/RAVE/src/RAVE/face_detection/Direction2Pixel.py
+++ library/RAVE/src/RAVE/face_detection/Direction2Pixel.py
@@ -104,8 +104,75 @@ class Direction2Pixel:
         )
 
 
+class Direction2PixelFrom3D:
+    def __init__(
+        self,
+        img_height=480,
+        img_width=640,
+    ):
+        self._center_x = img_width / 2
+        self._center_y = img_height / 2
+        # self._x_offset = x_offset
+        # self._y_offset = y_offset
+
+        self._k_matrix = np.array(
+            [
+                [376.96798, 0.0, 314.09011],
+                [0.0, 374.08737, 250.37452],
+                [0.0, 0.0, 1.0],
+            ]
+        )
+        self._translation_eye_camera = np.array([-0.031, -0.032, 0])
+        self._distortion = np.array([-0.321459, 0.073634])
+
+        self._eps = 1e-20
+
+    def get_pixel(self, angle_x, angle_y, dist):
+        angle_y = -1 * angle_y
+        x_eye = dist * np.sin(np.deg2rad(angle_x))
+        y_eye = dist * np.tan(np.deg2rad(angle_y))
+        z_eye = dist * np.cos(np.deg2rad(angle_x))
+
+        x_cam = x_eye + self._translation_eye_camera[0]
+        y_cam = y_eye + self._translation_eye_camera[1]
+        z_cam = z_eye + self._translation_eye_camera[2]
+
+        a = x_cam / z_cam
+        b = y_cam / z_cam
+
+        r = np.sqrt(a ** 2 + b ** 2)
+        theta = np.arctan(r)
+
+        theta_distorted = theta * (
+            1
+            + (self._distortion[0] * (theta ** 2))
+            + (self._distortion[1] * (theta ** 4))
+        )
+
+        x_cam = theta_distorted * a / (r + self._eps)
+        y_cam = theta_distorted * b / (r + self._eps)
+
+        pixel_x = self._k_matrix[0, 0] * x_cam + self._k_matrix[0, 2]
+        pixel_y = self._k_matrix[1, 1] * y_cam + self._k_matrix[1, 2]
+
+        if pixel_x > self._center_x * 2:
+            pixel_x = self._center_x * 2
+        elif pixel_x < 0:
+            pixel_x = 0
+
+        if pixel_y > self._center_y * 2:
+            pixel_y = self._center_y * 2
+        elif pixel_y < 0:
+            pixel_y = 0
+
+        return (
+            int(pixel_x),
+            int(pixel_y),
+        )
+
+
 if __name__ == "__main__":
-    converter = Direction2Pixel(-16, 21)
+    converter = Direction2PixelFrom3D()
 
     # start = time.time()
     # it = 100000
@@ -128,5 +195,5 @@ if __name__ == "__main__":
     #
     # plt.imshow(x_plot, interpolation='nearest')
     # plt.show()
-    x, y = converter.get_pixel(-15, 8.51)
+    x, y = converter.get_pixel(-25, 5.64, 1.77)
     print(f"Point: {x}, {y}")
