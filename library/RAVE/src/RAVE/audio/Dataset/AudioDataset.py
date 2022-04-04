20ed215374a00b20cc6951d70a558399cd43c04b
Felix <ducf2605@usherbrooke.ca>
2022-04-04 15:06:10 -0400

    change target for rnn

--- library/RAVE/src/RAVE/audio/Dataset/AudioDataset.py
+++ library/RAVE/src/RAVE/audio/Dataset/AudioDataset.py
@@ -19,19 +19,20 @@ from RAVE.audio.Beamformer.Beamformer import Beamformer
 MINIMUM_DATASET_LENGTH = 500
 IS_DEBUG = True
 
+
 class AudioDataset(torch.utils.data.Dataset):
     """
     Class handling usage of the audio dataset
     """
 
     def __init__(
-        self,
-        dataset_path=None,
-        data_split=None,
-        generate_dataset_runtime=False,
-        is_debug=False,
-        sample_rate = 16000,
-        num_samples = 32000
+            self,
+            dataset_path=None,
+            data_split=None,
+            generate_dataset_runtime=False,
+            is_debug=False,
+            sample_rate=16000,
+            num_samples=32000
     ):
         self.duration = num_samples / sample_rate
         self.is_debug = IS_DEBUG
@@ -47,22 +48,12 @@ class AudioDataset(torch.utils.data.Dataset):
             except yaml.YAMLError as exc:
                 print(exc)
 
-        
-
         # Set up dataset (if no folder, open tkinter to choose)
         self.dataset_path = dataset_path
         if not self.dataset_path:
             self.dataset_path = filedialog.askdirectory(title="Choose a dataset folder")
         self.data = []
 
-        # If need generator, get item
-        self.dataset_builder = AudioDatasetBuilder(self.configs['source_folder'],
-                                                   self.configs['noise_folder'],
-                                                   self.dataset_path,
-                                                   self.configs['noise_count_range'],
-                                                   self.configs['speech_as_noise'],
-                                                   self.configs['sample_per_speech'],
-                                                   self.is_debug)
         self.get_dataset(self.dataset_path)
 
         self.generate_dataset_runtime = generate_dataset_runtime
@@ -74,14 +65,13 @@ class AudioDataset(torch.utils.data.Dataset):
 
         self.transformation = torchaudio.transforms.Spectrogram(
             n_fft=1024,
-            hop_length= self.hop_len,
+            hop_length=self.hop_len,
             power=None,
             return_complex=True,
-            normalized=True
         )
         self.waveform = torchaudio.transforms.InverseSpectrogram(
-             n_fft=1024,
-            hop_length= 256
+            n_fft=1024,
+            hop_length=256
         )
         self.delay_and_sum = Beamformer('delaysum', frame_size=1024)
 
@@ -89,91 +79,92 @@ class AudioDataset(torch.utils.data.Dataset):
         return len(self.data)
 
     def __getitem__(self, idx):
-        """if self.generate_dataset_runtime:
-            audio_signal, audio_mask, speech_mask, noise_mask, config_dict = self.run_dataset_builder() # todo: audio_signal needs to be a tensor 
-        else:"""
         item_path = self.data[idx]
-        audio_signal, audio_sr, noise_target, noise_sr, speech_target, speech_sr, config_dict = self.load_item_from_disk(item_path)
+        audio_signal, audio_sr, noise_target, noise_sr, speech_target, speech_sr, config_dict = self.load_item_from_disk(
+            item_path)
 
         min_val = min(audio_signal.shape[1], noise_target.shape[1], speech_target.shape[1])
+        begin = 0#random.randint(0, (min_val - self.num_samples)) if min_val > self.num_samples + 1 else 0
 
-        begin = random.randint(0, (min_val - self.num_samples)) if min_val > self.num_samples + 1 else 0
-
-        signal1 = self._formatAndConvertToSpectogram(audio_signal, audio_sr, begin)
+        signal1 = self.signal1(audio_signal, audio_sr, begin)
         signal2 = self._delaySum(audio_signal, audio_sr, config_dict, begin)
-        noise_target = self._formatAndConvertToSpectogram(noise_target, noise_sr, begin)
-        speech_target = self._formatAndConvertToSpectogram(speech_target, speech_sr, begin)
-
         signal = torch.cat([signal1, signal2], dim=1)
 
-        noise_target -= torch.min(noise_target)
-        speech_target -= torch.min(speech_target)
+        noise_target = self.targets(noise_target, noise_sr, begin)
+        speech_target = self.targets(speech_target, speech_sr, begin)
+        #noise_target -= torch.min(noise_target)
+        #speech_target -= torch.min(speech_target)
 
+        #target = noise_target ** 2 / (noise_target ** 2 + speech_target ** 2 + 1e-10)
+        target = noise_target / (noise_target + speech_target + 1e-10)
 
-        target = noise_target**2 / (noise_target**2 + speech_target**2 + 1e-10)
+        total_energy = noise_target + speech_target
 
-        total_energy =noise_target**2 + speech_target**2
-        total_energy -= torch.min(total_energy)
-        total_energy /= torch.max(total_energy)
 
         return signal, torch.squeeze(target), total_energy
-    
+
+    def signal1(self, raw_signal, sr, begin):
+        signal = self._resample(raw_signal, sr)
+        signal = self._cut(signal, begin)
+        signal = self._right_pad(signal)
+        freq_signal = self.transformation(signal)
+        #freq_signal = freq_signal ** 2
+        freq_signal = self._set_mono(freq_signal)
+        freq_signal = 20 * torch.log10(torch.abs(freq_signal) + 1e-09) # Garder ca
+        #freq_signal = 20 * torch.log10(freq_signal + 1e-09)
+        return freq_signal
+
+    def targets(self, raw_signal, sr, begin):
+        signal = self._resample(raw_signal, sr)
+        signal = self._cut(signal, begin)
+        signal = self._right_pad(signal)
+        freq_signal = self.transformation(signal)
+        #freq_signal = freq_signal.real
+        freq_signal = torch.abs(freq_signal) ** 2
+        freq_signal = self._set_mono(freq_signal)
+        #freq_signal = 20 * torch.log10(torch.abs(freq_signal) + 1e-09)
+        #freq_signal = freq_signal.real
+        return freq_signal
+
     def _delaySum(self, raw_signal, sr, config, begin):
         signal = self._resample(raw_signal, sr)
         signal = self._cut(signal, begin)
         signal = self._right_pad(signal)
         freq_signal = self.transformation(signal)
-        mic0 = list(np.subtract(config['microphones'][0], config['user_pos']))
-        mic1 = list(np.subtract(config['microphones'][1], config['user_pos']))
-        mic2 = list(np.subtract(config['microphones'][2], config['user_pos']))
-        mic3 = list(np.subtract(config['microphones'][3], config['user_pos']))
-        
-        mic_array =  generate_mic_array({
+        mic = config['mic_rel']
+
+        mic_array = generate_mic_array({
             "mics": {
-                "0": mic0,
-                "1": mic1,
-                "2": mic2,
-                "3": mic3
+                "0": mic[0],
+                "1": mic[1],
+                "2": mic[2],
+                "3": mic[3],
+                "4": mic[4],
+                "5": mic[5],
+                "6": mic[6],
+                "7": mic[7]
             },
-            "nb_of_channels": 4
+            "nb_of_channels": 8
         })
 
         X = freq_signal.cpu().detach().numpy()
         X = np.einsum("ijk->kij", X)
-        delay = np.squeeze(get_delays_based_on_mic_array(np.array([config['source_dir']]), mic_array, 1024)) #set variable 1024
-        
-        signal = np.zeros((1, X.shape[2], X.shape[0]),dtype=complex)
+        delay = np.squeeze(
+            get_delays_based_on_mic_array(np.array([config['source_dir']]), mic_array, 1024))  # set variable 1024
+
+        signal = np.zeros((1, X.shape[2], X.shape[0]), dtype=complex)
         for index, item in enumerate(X):
-            signal[...,index] = self.delay_and_sum(freq_signal=item, delay=delay)
+            signal[..., index] = self.delay_and_sum(freq_signal=item, delay=delay)
 
         signal = torch.from_numpy(signal)
-        signal = 20*torch.log10(torch.abs(signal) + 1e-09)
+        signal = 20 * torch.log10(torch.abs(signal) + 1e-09) # Garder ca
+        #signal = 20 * torch.log10(signal**2 + 1e-09)
         return signal
-
-    def _formatAndConvertToSpectogram(self, raw_signal, sr, begin):
-        signal = self._resample(raw_signal, sr)
-        signal = self._cut(signal, begin)
-        signal = self._right_pad(signal)
-        freq_signal = self.transformation(signal)
-        freq_signal = freq_signal**2
-        freq_signal = self._set_mono(freq_signal)
-        freq_signal = 20*torch.log10(torch.abs(freq_signal) + 1e-09)
-        return freq_signal
-
-    """def _formatAndConvertToSpectogram(self, raw_signal, sr):
-        signal = self._resample(raw_signal, sr)
-        signal = self._cut(signal)
-        signal = self._right_pad(signal)
-
-        signal = self._set_mono(signal)
-        freq_signal = self.transformation(signal)
-        freq_signal = 20*torch.log10(torch.abs(freq_signal) + 1e-09)
-        return freq_signal"""
 
     def _resample(self, signal, sr):
         if sr != self.sample_rate:
             resampler = torchaudio.transforms.Resample(sr, self.sample_rate)
-            signal =resampler(signal)
+            signal = resampler(signal)
         return signal
 
     @staticmethod
@@ -183,11 +174,11 @@ class AudioDataset(torch.utils.data.Dataset):
         return signal
 
     def _cut(self, signal, begin):
-        #begin = 0
+        # begin = 0
         if signal.shape[1] > self.num_samples:
-            signal = signal[:, begin: begin+self.num_samples]
+            signal = signal[:, begin: begin + self.num_samples]
         return signal
-    
+
     def _right_pad(self, signal):
         length_signal = signal.shape[1]
         if length_signal < self.num_samples:
