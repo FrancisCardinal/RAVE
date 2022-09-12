2e322506ed5536aa037b77206fb16166c5d19c16
FrancisCardinal <francis.cardinal.01@gmail.com>
2022-09-12 15:07:55 -0400

    Changed DANNTrainer so that it displays the 2 sublosses

--- library/RAVE/src/RAVE/common/DANNTrainer.py
+++ library/RAVE/src/RAVE/common/DANNTrainer.py
@@ -1,6 +1,11 @@
+import os
 import torch
 import numpy as np
 from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+from time import localtime, strftime, time
+from datetime import timedelta
 
 from RAVE.common.Trainer import Trainer
 
@@ -58,6 +63,79 @@ class DANNTrainer(Trainer):
 
         self.domain_classification_loss_function = torch.nn.BCEWithLogitsLoss()
 
+        self.training_regression_losses, self.validation_regression_losses = [], []
+        self.training_domain_losses, self.validation_domain_losses = [], []
+
+        self.figure, (self.ax1, self.ax2) = plt.subplots(2, 1, sharey=False)
+        plt.ion()
+        self.figure.show()
+
+
+    def train_with_validation(self, NB_EPOCHS):
+        """
+        Main method of the class, used to train the model.
+        Args:
+            NB_EPOCHS (int): Number of epoch for which to train the model
+        """
+        self.NB_EPOCHS = NB_EPOCHS
+        start_time = time()
+
+        self.epoch = 0
+        while (self.epoch < self.NB_EPOCHS) and (not self.terminate_training):
+            current_regression_training_loss, current_domain_training_loss = self.compute_training_loss()
+            current_regression_validation_loss, current_domain_validation_loss = self.compute_validation_loss()
+
+            self.training_regression_losses.append(current_regression_training_loss)
+            self.validation_regression_losses.append(current_regression_validation_loss)
+
+            self.training_domain_losses.append(current_domain_training_loss)
+            self.validation_domain_losses.append(current_domain_validation_loss)
+            self.update_plot()
+
+            epoch_stats = (
+                f"Epoch {self.epoch:0>4d} | "
+                f"validation_loss={current_regression_validation_loss:.6f} | "
+                f"training_loss={current_regression_training_loss:.6f}"
+            )
+
+            if self.min_validation_loss > current_regression_validation_loss:
+                epoch_stats = epoch_stats + (
+                    "  | Min validation loss decreased("
+                    f"{self.min_validation_loss:.6f}--->"
+                    f"{current_regression_validation_loss:.6f}) : Saved the model"
+                )
+                self.min_validation_loss = current_regression_validation_loss
+
+                self.save_model_and_training_info()
+
+            print(epoch_stats)
+            self.scheduler.step(current_regression_validation_loss)
+            self.epoch += 1
+
+        self.terminate_training = True
+        min_training_loss = min(self.training_regression_losses)
+        time_of_completion = strftime("%Y-%m-%d %H:%M:%S", localtime())
+        ellapsed_time = str(timedelta(seconds=(time() - start_time)))
+        figure_title = (
+            f"{time_of_completion:s} | "
+            f"ellapsed_time={ellapsed_time:s} | "
+            f"min_validation_loss={self.min_validation_loss:.6f} | "
+            f"min_training_loss={min_training_loss:.6f}"
+        )
+
+        print(figure_title)
+        plt.savefig(
+            os.path.join(
+                self.ROOT_DIR_PATH,
+                Trainer.TRAINING_SESSIONS_DIR,
+                figure_title + ".png",
+            ),
+            dpi=200,
+        )
+        plt.close(None)
+        return self.min_validation_loss
+
+
     def compute_training_loss(self):
         """
         Compute the training loss for the current epoch
@@ -67,7 +145,7 @@ class DANNTrainer(Trainer):
         """
         self.model.train()
 
-        training_loss = 0.0
+        domain_training_loss, regression_training_loss = 0.0, 0.0
         number_of_images = 0
         len_dataloader = len(self.training_loader)
         i = 0
@@ -94,21 +172,23 @@ class DANNTrainer(Trainer):
             # Forward Pass
             predictions, classifications = self.model(images, 2 * alpha)
             # Find the Loss
-            loss = self.loss_function(predictions, labels)
-            domain_loss = self.domain_classification_loss_function(
+            regression_loss = self.loss_function(predictions, labels)
+            domain_loss = 100.0*self.domain_classification_loss_function(
                 classifications, domains.unsqueeze(1)
             )
-            loss += domain_loss
+            loss = regression_loss + domain_loss
             # Calculate gradients
             loss.backward()
             # Update Weights
             self.optimizer.step()
             # Calculate Loss
-            training_loss += loss.item()
+            regression_training_loss += regression_loss.item()
+            domain_training_loss += domain_loss.item()
+
             number_of_images += len(images)
             i += 1
 
-        return training_loss / number_of_images
+        return regression_training_loss / number_of_images, domain_training_loss / number_of_images
 
     def compute_validation_loss(self):
         """
@@ -120,7 +200,7 @@ class DANNTrainer(Trainer):
         with torch.no_grad():
             self.model.eval()
 
-            validation_loss = 0.0
+            domain_validation_loss, regression_validation_loss = 0.0, 0.0
             number_of_images = 0
             len_dataloader = len(self.validation_loader)
             i = 0
@@ -144,14 +224,50 @@ class DANNTrainer(Trainer):
                 # Forward Pass
                 predictions, classifications = self.model(images, 2 * alpha)
                 # Find the Loss
-                loss = self.loss_function(predictions, labels)
-                domain_loss = self.domain_classification_loss_function(
+                regression_loss = self.loss_function(predictions, labels)
+                domain_loss = 100.0*self.domain_classification_loss_function(
                     classifications, domains.unsqueeze(1)
                 )
-                loss += domain_loss
                 # Calculate Loss
-                validation_loss += loss.item()
+                regression_validation_loss += regression_loss.item()
+                domain_validation_loss += domain_loss.item()
                 number_of_images += len(images)
                 i += 1
 
-            return validation_loss / number_of_images
+            return regression_validation_loss / number_of_images, domain_validation_loss / number_of_images
+
+
+    def update_plot(self):
+        """
+        Updates the plot at the end of an epoch to show all of the training
+        losses and validation losses computed so far
+        """
+        self.ax1.clear()
+        self.ax2.clear()
+
+        self.ax1.plot(
+            range(len(self.training_regression_losses)),
+            self.training_regression_losses,
+            label="training regression loss",
+        )
+        self.ax1.plot(
+            range(len(self.validation_regression_losses)),
+            self.validation_regression_losses,
+            label="validation regression loss",
+        )
+
+        self.ax2.plot(
+            range(len(self.training_regression_losses)),
+            self.training_domain_losses,
+            label="training domain loss",
+        )
+        self.ax2.plot(
+            range(len(self.validation_regression_losses)),
+            self.validation_domain_losses,
+            label="validation domain loss",
+        )
+        self.ax1.legend(loc="upper left")
+        self.ax2.legend(loc="upper left")
+
+        self.figure.canvas.draw()
+        plt.pause(0.001)
