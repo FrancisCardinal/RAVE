c4006178b16166a1c8c02f230b39b46eac4c5ba5
Anthony Gosselin <anthony.gosselin2@usherbrooke.ca>
2022-11-16 16:01:55 -0500

    Fixes to run vision on computer

--- runRaveWeb.ps1
+++ runRaveWeb.ps1
@@ -1,4 +1,4 @@
 # Runs the rave-web suite calling each script individually
 start powershell { npm run --prefix ./rave-server dev; Read-Host }
 start powershell { npm run --prefix ./rave-web start; Read-Host }
-start powershell { cd ./library/RAVE/src; python main_vision_web.py --freq 0.75; Read-Host }
+start powershell { cd ./library/RAVE/src; python main_vision_web.py --freq 0.75 --headless; Read-Host }
